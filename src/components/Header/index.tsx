4acdca216e385fe88d907b0f54d13dd25d3078df
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-03-28 19:56:33 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  // TO-DO: Logo redirect to home page.
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/LogoCompleted.svg" alt="Space Traveling Logo" />
+      </div>
+    </header>
+  );
 }
